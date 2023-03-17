65bca9a263fc2a473fa38721dcbd2bbabc3ab433
Ot-Sara <saraotmani95@gmail.com>
2023-03-17 13:20:19 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,18 +12,18 @@ int main(void)
 
 	for (n = 0; n < 98; n++)
 	{
-	for (m = n + 1; m <= 99; m++)
-	{
-	putchar((n / 10) + '0');
-	putchar((n % 10) + '0');
-	putchar(' ');
-	putchar((m / 10) + '0');
-	putchar((m % 10) + '0');
-	if (n == 98 && m == 99)
-	continue;
-	putchar(',');
-	putchar(' ');
-	}
+		for (m = n + 1; m <= 99; m++)
+		{	
+			putchar((n / 10) + '0');
+			putchar((n % 10) + '0');
+			putchar(' ');
+			putchar((m / 10) + '0');
+			putchar((m % 10) + '0');
+			if (n == 98 && m == 99)
+				continue;
+		putchar(',');
+		putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
