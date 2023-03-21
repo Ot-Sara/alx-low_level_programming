e58f831b3e496a4379371b6cc99b8d621571b5c5
Ot-Sara <saraotmani95@gmail.com>
2023-03-21 12:00:48 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - Shows computes the absolute value
+ * of the integer argument j
+ *
+ * @j: the input value as an integer.
+ *
+ * Return: 1 if j is greater than
+ * 0. 0 for else.
+ */
+int _abs(int j)
+{
+
+	if (j > 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
