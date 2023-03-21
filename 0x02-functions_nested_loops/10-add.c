c403e712930d2ca63c8b7faeb1b0c02fb01d2fb1
Ot-Sara <saraotmani95@gmail.com>
2023-03-21 15:13:53 -0700

    times table

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ *Return: the result
+ */
+int main(void)
+int sum  add_numbers(int a, int b)
+{
+	int sum;
+	sum = a + b;
+	return sum;
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -31,7 +31,8 @@ void times_table(void)
 				_putchar(32);
 				_putchar(c + '0');
 			}
+			if (b = 9)
+				_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
