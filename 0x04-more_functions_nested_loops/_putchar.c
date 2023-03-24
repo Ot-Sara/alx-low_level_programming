b0d280f2d4e2e5817ef27fee4f652ca80dd16748
Ot-Sara <saraotmani95@gmail.com>
2023-03-23 18:05:08 -0700

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,7 +1,8 @@
-#include "main.h"
 #include <unistd.h>
+
 /**
- * _putchar - writes the character c to stdout
+ * _putchar - prints the character c to stdout
+ *
  * @c: The character to print
  *
  * Return: On success 1.
