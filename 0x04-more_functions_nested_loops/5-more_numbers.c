3e944d35e4d04e459d5f62d030c9251377f7b852
Ot-Sara <saraotmani95@gmail.com>
2023-03-24 07:16:36 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,7 @@ void more_numbers(void)
 
 	for (a = 0; a < 10; a++)
 	{
-	       for (b = 0; b <= 14; b++)
+		for (b = 0; b <= 14; b++)
 		{
 			if (b >= 10)
 			{
@@ -19,6 +19,6 @@ void more_numbers(void)
 			}
 			_putchar(b % 10 + '0');
 		}
-	       _putchar('\n');
+		_putchar('\n');
 	}
 }
