bb711c29843264f398deb894e7596771f8323b39
Ot-Sara <saraotmani95@gmail.com>
2023-03-30 14:05:22 -0700

    rot 13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - rotates by 13 places
+ *
+ * @s: input string
+ *
+ * Return: the pointer
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	char rot13[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char ROT13[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	char *ptr = s;
+
+	while (*s)
+	{
+		for (i = 0; i <= 52; i++)
+		{
+			if (*s == rot13[i])
+			{
+				*s = ROT13[i];
+				break;
+			}
+		}
+		s++;
+	}
+	return (ptr);
+}
