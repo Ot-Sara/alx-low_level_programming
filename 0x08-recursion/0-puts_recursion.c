b0c51daaf65d91c8ce6adfeb89b3a29bc9756e12
Ot-Sara <saraotmani95@gmail.com>
2023-04-05 09:30:04 -0700

    puts recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -10,9 +10,11 @@
 
 void _puts_recursion(char *s)
 {
-	while(*s != '\0')
+	if(*s)
 	{
-		_putchar(*s++);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
