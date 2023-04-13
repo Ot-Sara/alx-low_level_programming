e128c06feb2c9195bbaaed8587d8578065071697
Ot-Sara <saraotmani95@gmail.com>
2023-04-13 08:26:19 -0700

    malloc checked

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -4,16 +4,13 @@
 /**
  * malloc_checked - allocates memory using malloc
  * @b: the input integer
- * @ptr: the pointer
  * Return: a pointer to the allocated memory.
  * 98 if malloc fails.
  */
 
 void *malloc_checked(unsigned int b)
 {
-	void *ptr;
-
-	ptr = malloc(b);
+	int *ptr = malloc(b);
 
 	if (ptr == NULL)
 		exit(98);
