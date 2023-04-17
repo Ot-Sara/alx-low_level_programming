dda95107b6e80da7f231b1ea5ca94347413e16fe
Ot-Sara <saraotmani95@gmail.com>
2023-04-17 16:15:59 -0700

    2 main.c

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,15 @@
+#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
