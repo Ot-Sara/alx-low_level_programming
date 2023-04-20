1d58d3ec1abf1697be1df4ab19661adcfa4e2b20
Ot-Sara <saraotmani95@gmail.com>
2023-04-19 18:32:46 -0700

    operation functions

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "3-calc.h"
+
+/**
+ * main - prints the number of arguments passed into it
+ * @num1: number of arguments
+ * @num2: array of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int __attribute__((__unused__))argc, char *argv[])
+{
+	int num1, num2;
+	char *op;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	op = argv[2];
+
+	if (get_op_func(op) == NULL || op[1] != '\0')
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	printf("%d\n", get_op_func(op)(num1, num2));
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds two numbers together
+ * a: the first number
+ * b: the second number
+ * return: the result
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - substracts two numbers together
+ * a: the first number
+ * b: the second number
+ * return: the result
+ */
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies two numbers together
+ * a: the first number
+ * b: the second number
+ * return: the result
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divises two numbers together
+ * a: the first number
+ * b: the second number
+ * return: the result
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modules two numbers together
+ * a: the first number
+ * b: the second number
+ * return: the result
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
