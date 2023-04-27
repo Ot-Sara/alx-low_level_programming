00b5417148be2735fbe70d556d0a1c7b68a6637c
Ot-Sara <saraotmani95@gmail.com>
2023-04-27 16:40:46 -0700

    print list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -6,6 +6,8 @@
  * @str: string - (malloced string)
  * @len: length of the string
  * @next: points to the next node
+ *
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
