58790e3638ed534c5a54e3edf2a0e39d3fae4c7d
Ot-Sara <saraotmani95@gmail.com>
2023-05-08 15:19:57 -0700

    read textfile

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -37,7 +37,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 			return (0);
 		bytes_read += n;
 		bytes_written += m; }
-	if (close(file_descriptor) == -1 || write(STDOUT_FILENO, buffer, bytes_read)
-			!= (ssize_t) bytes_read)
+	if (close(file_descriptor) == -1)
 		return (0);
 	return (bytes_written); }
